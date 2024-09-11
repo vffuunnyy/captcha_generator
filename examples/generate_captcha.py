c6f952462714c7968ebe700c3f95b9ab8c7f26ac
vffuunnyy <vffuunnyy@gmail.com>
2024-09-11 17:52:45 +0700

    add example

---
+++ examples/generate_captcha.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from captcha_generator import CaptchaGenerator
+
+
+emojis = Path("./emojis")
+generator = CaptchaGenerator(emojis, "png")
+res = generator.generate(5, 10)
+
+print(f"Correct Emoji: {res.correct_emoji}")
+print(f"Emojis on Image: {res.image_emojis}")
+print(f"Emoji Options: {res.keyboard_emojis}")
+
+Path("captcha.png").write_bytes(res.image)
